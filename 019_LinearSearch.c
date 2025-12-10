768c3797e113a1d4f2a39815a3f651b67472f8fe
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2025-12-10 19:56:28 +0530

    Linear Search

---
+++ 019_LinearSearch.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int linearSearch(int arr[],int size,int element){
+    for(int i = 0;i<size;i++){
+        if(arr[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={12,233,21,324,5432,423,24,13,5,123,6756};
+    int size = sizeof(arr)/sizeof(int);
+    int element;
+
+    printf("Enter the Searching Element: ");
+    scanf("%d",&element);
+
+    int searchIndex = linearSearch(arr, size, element);
+
+    printf("The Element %d is found in index %d \n",element,searchIndex);
+
+    return 0;
+}

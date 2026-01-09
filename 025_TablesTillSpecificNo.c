d02570009e8be88d133388faf9980061e3efe0d5
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2026-01-09 22:57:46 +0530

    tables till specific No. using nested loop

--- 025_TablesTillSpecificNo.c
+++ 025_TablesTillSpecificNo.c
@@ -11,6 +11,7 @@ int main(){
             for(int j=1;j<=10;j++){
                 printf("%d X %d = %d \n",i,j,i*j);
             }
+            printf("-----------XXXX---------------- \n");
         }
     }
 

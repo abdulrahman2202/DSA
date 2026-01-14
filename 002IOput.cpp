54fa96fc529062ba284cfb9ee188be107e5010a1
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2026-01-14 22:29:10 +0530

    Taking User Input

--- 002IOput.cpp
+++ 002IOput.cpp
@@ -3,8 +3,8 @@
 int main(){
  
  int a;
- printf("Enter the number :");
+ printf("Enter the number : ");
  scanf("%d",&a);
- printf("the value of a is %d",a);
+ printf("The value of a is %d",a);
     return 0;
 }
